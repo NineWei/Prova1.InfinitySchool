1d613e7899c30e723f6f508b2b12eedeb49db28b
Winnie Wei <104879662+NineWei@users.noreply.github.com>
2024-08-18 00:37:39 -0300

    Add files via upload
    Prova 1 - Infinity School
    Programa para calcular a área de um retângulo.

---
+++ Prova 1. Calculo Retangulo.py
@@ -0,0 +1,9 @@
+base = input("Digite a base do retângulo: ")
+altura = input("Digite a altura do retângulo: ")
+
+base = float(base)
+altura = float(altura)
+
+area = base * altura
+
+print(f"A área do retângulo é {area}.")
